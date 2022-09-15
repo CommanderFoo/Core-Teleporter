0cb0d0f033884398f434ab22bec4676a88f5199c
CommanderFoo <pixeldepth@gmail.com>
2022-09-15 16:34:15 +0100

    Update README.lua

--- Data/Scripts/README.lua
+++ Data/Scripts/README.lua
@@ -10,7 +10,7 @@
 ----------------------------------------------------
 
 The Teleporter component allows creators to set up teleporters around their map to help
-players quick travel between different areas.
+players quickly travel between different areas.
 
 The component uses a data table to make connecting teleporters quick and simple.
 -----------------------------------------------------------------------------------------------------------------
@@ -20,27 +20,27 @@ How to use
 ==========
 
 An example template is included that contains 2 teleporters that are linked to each other. When
-the player enters the outside teleporter, the player will be teleportered to the inside teleporter
+the player enters the outside teleporter, the player will be teleported to the inside teleporter
 in the cafe. 
 
-To create your own teleporters, there is a Teleporter template that can be used. The template containts
+To create your own teleporters, there is a Teleporter template that can be used. The template contains
 2 custom properties on the root of the template.
 
 - ShowName
   If enabled, the name will appear above the teleporter for the player.
 
 - Name
-  The name of the teleporter being transferred to.
+  The name of the teleporter that will be transferred to.
 
 ======================
 Connecting Teleporters
 ======================
 
-Connecting up teleporters is very easy.
+Connecting teleporters is very easy.
 
 In Project Content find the Teleporters data table and open it. You will see 3 columns, From, To, and TwoWay.
 Add teleporters to the From and To column, the TwoWay column can be enabled if players can go back through
 the teleporter.
 
-Teleporters can be chained very easily. For example, going from A to B, B to C and so on.
+Teleporters can be chained easily. For example, from A to B, B to C, and so on.
 --]]
